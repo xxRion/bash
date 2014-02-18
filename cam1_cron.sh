8965a80a31114fc52f55f790ea0fae43ae7addac
Rickard Fransson <rickard.fransson@webben.nu>
2014-02-18 20:44:45 +0100

    cron-script igelösa

---
+++ cam1_cron.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FOLDER="/home/cam1"
+NAME=$(date -d yesterday +"%Y%m%d")
+
+#Create directory and move files
+mkdir $FOLDER/$NAME
+mv $FOLDER/bilder/ig${NAME}_*.jpg $FOLDER/$NAME
+
+#Create tar-ball for transfer to nasa
+tar zcf $FOLDER/$NAME.tgz $FOLDER/$NAME
+scp $FOLDER/$NAME.tgz rickard@webben.nu:/home/rickard/igelosa/cam1
+
+#Remove old files
+FILE=$FOLDER/$(date -d "3 days ago" +"%Y%m%d")
+if [ -f ${FILE}.tgz ]
+  then
+    rm -rf ${FILE}*
+fi
+
---
+++ cam3_cron.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FOLDER="/home/cam3"
+NAME=$(date -d yesterday +"%Y%m%d")
+
+#Create directory and move files
+mkdir $FOLDER/$NAME
+find $FOLDER/bilder/ -name *.jpg -exec mv {} $FOLDER/$NAME \;
+
+#Create tar-ball for transfer to nasa
+tar zcf $FOLDER/$NAME.tgz $FOLDER/$NAME
+scp $FOLDER/$NAME.tgz rickard@webben.nu:/home/rickard/igelosa/cam3
+
+#Remove old files
+FILE=$FOLDER/$(date -d "3 days ago" +"%Y%m%d")
+if [ -f ${FILE}.tgz ]
+  then
+    rm -rf ${FILE}*
+fi
+
